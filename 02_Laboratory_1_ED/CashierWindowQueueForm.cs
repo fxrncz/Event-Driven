3092ccf15681bfceab0ddff4d48fd978a7568cab
Francis Oliver <121164562+fxrncz@users.noreply.github.com>
2024-09-15 20:01:21 +0800

    Add files via upload

---
+++ 02_Laboratory_1_ED/CashierWindowQueueForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_Laboratory_1_ED
+{
+
+    public partial class CashierWindowQueue : Form
+    {
+        private Timer timer;
+
+        public CashierWindowQueue()
+        {
+            InitializeComponent();
+
+            timer = new Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (CashierClass.CashierQueue.Count > 0)
+            {
+                CashierClass.CashierQueue.Dequeue();
+                DisplayCashierQueue(CashierClass.CashierQueue);
+            }
+            else
+            {
+                MessageBox.Show("Queue is empty.");
+            }
+        }
+
+        public void DisplayCashierQueue(System.Collections.IEnumerable CashierList)
+        {
+            listCashierQueue.Items.Clear();
+            foreach (Object obj in CashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
+        }
+    }
+}
---
+++ 02_Laboratory_1_ED/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_Laboratory_1_ED
+{
+    public partial class QueueingForm : Form
+    {
+        private CashierClass cashier;
+       
+        public QueueingForm()
+        {
+            InitializeComponent();
+            cashier = new CashierClass();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            lblQueue.Text = cashier.CashierGeneratedNumber("P - ");
+            CashierClass.getNumberInQueue = lblQueue.Text;
+            CashierClass.CashierQueue.Enqueue(CashierClass.getNumberInQueue);
+
+            CashierWindowQueue cashierWindow = new CashierWindowQueue();
+            cashierWindow.Show();
+
+        }
+    }
+}
